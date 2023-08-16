8c1e0df016f96c730f30d0e9d7e7fc8d91a5ba73
damian-aiv <damian@aiv-automatisering.nl>
2023-08-16 10:10:31 +0200

    Adding media library entry component

--- packages/spatie-laravel-media-library-plugin/resources/views/components/spatie-media-library-entry.blade.php
+++ packages/spatie-laravel-media-library-plugin/resources/views/components/spatie-media-library-entry.blade.php
@@ -5,7 +5,7 @@
     @foreach ($records as $record)
         <li class="block flex items-center justify-between text-sm">
             <a href="{{ $record->getUrl() }}" target="_blank" class="w-0 flex-1 flex items-center">
-                <x-heroicon-o-paper-clip class="h-4 w-4 text-gray-400" />
+                @svg('heroicon-o-paper-clip', 'h-4 w-4 text-gray-400')
                 <span class="ml-2 flex-1 w-0 truncate"> {{ $record->file_name }} </span>
             </a>
         </li>
