f3b6da05ff35ae97b9ece5b90a16ba7310b3aba3
damian-aiv <damian@aiv-automatisering.nl>
2023-09-21 11:07:27 +0200

    Scroll to first error after validation

--- packages/panels/resources/views/components/form/index.blade.php
+++ packages/panels/resources/views/components/form/index.blade.php
@@ -1,5 +1,20 @@
 <form
-    x-data="{ isUploadingFile: false }"
+    x-data="{
+        isUploadingFile: false,
+        init() {
+            Livewire.hook('commit', ({ succeed }) => {
+                succeed(() => {
+                    this.$nextTick(() => {
+                        const firstErrorMessage = document.querySelector('.fi-fo-field-wrp-error-message')
+
+                        if (firstErrorMessage !== null) {
+                            firstErrorMessage.scrollIntoView({ block: 'center', inline: 'center' })
+                        }
+                    })
+                })
+            })
+        }
+    }"
     x-on:submit="if (isUploadingFile) $event.preventDefault()"
     x-on:file-upload-started="isUploadingFile = true"
     x-on:file-upload-finished="isUploadingFile = false"
