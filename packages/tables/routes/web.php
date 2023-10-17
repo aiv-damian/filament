d78e7fe4cdd88a4f8f49d923e0d9f1915096ca83
damian-aiv <damian@aiv-automatisering.nl>
2023-10-17 10:39:35 +0200

    Fix for popover route

--- packages/tables/routes/web.php
+++ packages/tables/routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('_filament/tables/popover', function (Request $request) {
+Route::post('_filament/tables/popover', function (Request $request) {
     $model = $request->get('modelType')::find($request->get('modelId'));
 
     return view($request->get('view'), [
