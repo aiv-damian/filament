b76f3ab3f45ce15c3a73639503382a0d70f19114
damian-aiv <damian@aiv-automatisering.nl>
2023-10-19 12:24:16 +0200

    Testing speed

--- packages/panels/resources/views/components/topbar/index.blade.php
+++ packages/panels/resources/views/components/topbar/index.blade.php
@@ -123,14 +123,14 @@
             {{ \Filament\Support\Facades\FilamentView::renderHook('panels::global-search.before') }}
 
             @if (filament()->isGlobalSearchEnabled())
-                @livewire(Filament\Livewire\GlobalSearch::class)
+                @livewire(Filament\Livewire\GlobalSearch::class, ['lazy' => true])
             @endif
 
             {{ \Filament\Support\Facades\FilamentView::renderHook('panels::global-search.after') }}
 
             @if (filament()->auth()->check())
                 @if (filament()->hasDatabaseNotifications())
-                    @livewire(Filament\Livewire\DatabaseNotifications::class)
+                    @livewire(Filament\Livewire\DatabaseNotifications::class, ['lazy' => true])
                 @endif
 
                 <x-filament-panels::user-menu />
